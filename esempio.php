2c8d2cb873480e316603ee76fb86d9353bdd9c96
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2023-12-20 11:12:53 +0100

    Titolo: PHP Base aggiornato

--- esempio.php
+++ esempio.php
@@ -1,5 +1,10 @@
 <?php
+    $stringa='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad atque officia autem voluptates minus totam sunt consequatur repellendus doloremque officiis aspernatur veritatis ipsum ab eligendi quas ipsa aliquid, in fuga!';
+    $nome='Luca';
+    echo strtoupper($stringa) ;
 
-    echo 'Ciao '. $_POST['nome'] . ' ' . $_POST['cognome']
 
 ?>
+<style>
+    *{background:black;color:white;}
+</style> 
--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     </head>
     <body>
         <h1>Form esempio</h1>
+        <?php $nome='Orazio'
+        ?>
+        <p>Nome <?php echo $nome;?></p>
         <form action="esempio.php" method="post">
             <input id="nome" name="nome" type="text" placeholder="Nome"><br>
             <input id="cognome" name="cognome"  type="text" placeholder="Cognome"><br>
