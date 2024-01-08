287b6fffa98b44170d8380533036b409f07938c5
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-01-08 10:42:18 +0100

    Titolo: Primo salvataggio

--- esempio.php
+++ esempio.php
@@ -1,51 +1,71 @@
-<?php
+<a href="http://127.0.0.1/tutorial_php/index.php">INDEX</a>
+<?php //fix ARRAY
+    echo'<h1>Array</h1><div>';
+    //todo Città
     $citta=['milano','firenze','roma'];    
-    echo '<br>Città <br>';
+    echo '<h2>Città </h2>';
     for ($i=0; $i<count($citta); $i++) { 
-        echo '- '.$citta[$i] . '<br>';
+        echo '<p>'.$i.') '.$citta[$i] . '</p>';
+        if($i==1){break;}
     }
-
-//todo array associativo
+    //todo array associativo e for each
     $persona=[
         'nome'=>'Luca',
         'cognome'=>'rossi',
         'eta'=>25,
-    ];echo '<br>Persona <br>';
+    ];
+    echo '<h2>Persona </h2>';
     foreach($persona as $chiave=>$valore){
-        echo $chiave.' => '.$valore."<br>";
+        echo'<p>'.$chiave.' => '.$valore."</p>";
     }
-
-//todo array bidimenzionale
+    //todo array bidimenzionale
     $scuola=[
         ['Luca','Marco','Anna'],
         ['Leonardo','Mattia','Paola'],
-    ];echo '<br>Scuola <br>';
+    ];echo '<h2>Scuola </h2>';
     for ($classe=0; $classe < count($scuola); $classe++) { 
-        echo '<br>'.'Classe '. $classe . '<br>';
-        for ($alunno=0; $alunno < count($scuola[$classe]); $alunno++) { 
+        echo '<p>'.'Classe '. $classe . '</p>';
+        for ($alunno=0; $alunno<count($scuola[$classe]); $alunno++) { 
             echo '- '. $scuola[$classe][$alunno].'<br>';
         }
     }
+    //todo ordinamento
+    echo '<h2>Ordinamento </h2>';
+    sort($citta); echo '<p><b>Sort</b>: ';print_r($citta);echo'</p>';
+    rsort($citta); echo '<p>Rsort: ';print_r($citta);echo'</p>';
 
-//todo ordinamento
-    echo '<br><br>Ordinamento <br>';
-    sort($citta); echo '<br> Sort: ';print_r($citta);
-    rsort($citta); echo '<br> Rsort: ';print_r($citta);
+    ksort($citta); echo '<p>Ksort: ';print_r($citta);echo'</p>';
+    krsort($citta); echo '<p>Krsort: ';print_r($citta);echo'</p>';
 
-
-    ksort($persona);echo '<br> Ksort: ';print_r($persona);
-    krsort($persona);echo '<br> Krsort: ';print_r($persona);
-
-    asort($persona);echo '<br> Asort: ';print_r($persona);
-    arsort($persona);echo '<br> Arsort: ';print_r($persona);
-//todo modifiche
-    echo '<br><br>Modifiche <br>';
-    array_push($citta, "Palermo");echo '<br> Shift: ';print_r($citta);
-    array_pop($citta);echo '<br> Pop: ';print_r($citta);
-    array_unshift($citta, "Catania");echo '<br> Shift: ';print_r($citta);
-    array_shift($citta);echo '<br> Pop: ';print_r($citta);
-
+    asort($citta); echo '<p>Asort: ';print_r($citta);echo'</p>';
+    arsort($citta); echo '<p>Arsort: ';print_r($citta);echo'</p>';
+    //todo modifiche
+    echo '<h2>Modifiche </h2>';
+    array_push($citta, "Palermo");  echo '<p>Push: ';print_r($citta);echo'</p>';
+    array_pop($citta);              echo '<p>Pop: ';print_r($citta);echo'</p>';
+    array_unshift($citta,'Catania');echo '<p>Unshift: ';print_r($citta);echo'</p>';
+    array_shift($citta);            echo '<p>Shift: ';print_r($citta);echo'</p></div>';
 ?>
+
+<?php //fix FUNZIONI
+    echo'<h1>Funzioni</h1><div>';
+    //todo FUNZIONE CON NOME
+    function saluta(){ echo '<p>Funzione con nome: OUTPUT</p>';}
+    saluta();
+    //todo FUNZIONE ANONIMA
+    $prova=function(){echo'<p>Funzione anonima: OUTPUT</p>';};
+    $prova();
+    //todo FUNZIONE CON PARAMETRI
+    function somma(int $v1, int $v2=11): int{
+        $somma=$v1+$v2;
+        return $somma;
+    }
+    $numero=10;  
+    echo'<p> Funzione con parametri: '.somma($numero,).'</p></div>';
+?>
+
+
 <style>
-    *{background:black;color:white;}
+    h1{border-bottom:1px solid white;}
+    *{background:#00000050; color:white; padding:4px;}
 </style> 
--- index.php
+++ index.php
@@ -6,14 +6,32 @@
     </head>
     <body>
         <h1>Form esempio</h1>
-        <?php $nome='Orazio'
-        ?>
-        <p>Nome <?php echo $nome;?></p>
+        <?php $nome='Mauro'?>
+        <p>
+            <a href="http://127.0.0.1/tutorial_php/esempio.php">index</a>
+        </p>
         <form action="esempio.php" method="post">
             <input id="nome" name="nome" type="text" placeholder="Nome"><br>
             <input id="cognome" name="cognome"  type="text" placeholder="Cognome"><br>
             <button type="submit">Invio</button>
         </form>
+
+<!-- todo IF HTML -->
+        <?php if($nome=='Orazio'): ?>
+        <p>Prima condizione verificata</p>
+        <?php elseif($nome=='Mauro'): ?>
+        <p>Seconda condizione verificata</p>
+        <?php else: ?>
+        <p>Condizione predefinita</p>
+        <?php endif; ?>
+
+<!-- todo LOOP HTML -->
+        <ul>
+            <?php for ($i=0;$i<6;$i++):?>
+                <li><?php echo $i?>) List items </li>
+            <?php endfor;?>
+        </ul>
+
     </body>
 </html>
 <style>
